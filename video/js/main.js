11820860b115f10cdc1acf46beb9291af8457c5c
Administrator <Administrator@SC-201711041011>
2018-10-19 16:54:27 +0800

    review tmp

---
+++ video/js/main.js
@@ -0,0 +1,80 @@
+;(function () {
+	
+	'use strict';
+
+	// iPad and iPod detection	
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function(){
+	    return (
+			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPod") != -1)
+	    );
+	};
+
+	// OffCanvass
+	var offCanvass = function() {
+		$('body').on('click', '.js-fh5co-menu-btn, .js-fh5co-offcanvass-close', function(){
+			$('#fh5co-offcanvass').toggleClass('fh5co-awake');
+		});
+	};
+
+	// Click outside of offcanvass
+	var mobileMenuOutsideClick = function() {
+		$(document).click(function (e) {
+	    var container = $("#fh5co-offcanvass, .js-fh5co-menu-btn");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	    	if ( $('#fh5co-offcanvass').hasClass('fh5co-awake') ) {
+	    		$('#fh5co-offcanvass').removeClass('fh5co-awake');
+	    	}
+	    }
+		});
+
+		$(window).scroll(function(){
+			if ( $(window).scrollTop() > 500 ) {
+				if ( $('#fh5co-offcanvass').hasClass('fh5co-awake') ) {
+		    		$('#fh5co-offcanvass').removeClass('fh5co-awake');
+		    	}
+	    	}
+		});
+	};
+
+	// Magnific Popup
+	var animateBoxWayPoint = function() {
+
+		if ($('.animate-box').length > 0) {
+			$('.animate-box').waypoint( function( direction ) {
+
+				if( direction === 'down' && !$(this).hasClass('animated') ) {
+					$(this.element).addClass('bounceIn animated');
+				}
+
+			} , { offset: '75%' } );
+		}
+
+	};
+
+	
+
+	
+	$(function(){
+		offCanvass();
+		mobileMenuOutsideClick();
+		animateBoxWayPoint();
+	});
+
+
+}());
+$(".play-btn").click(function(){
+ 
+    $("#frame").slideDown();
+    $("#signupFrame").attr("src","")
+    
+})
+$("#close").click(function(){
+    
+    $("#frame").slideUp();
+    
+})
